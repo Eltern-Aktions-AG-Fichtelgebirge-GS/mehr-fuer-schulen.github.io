68661585d7d9d90757594009fbe30b00b2b0974a
Andreas Zeidler <az@zitc.de>
2015-11-03 23:21:31 +0100

    die Seitentitel müssen auch übersetzbar sein...

--- site.js
+++ site.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 	$('#menu a').click(function(ev) {
 		var elem = $(this);
-		$('.content').hide();
-		$(elem.attr('href')).show();
+		$('.translation').hide();
+		$(elem.attr('href').replace('#', '.')).show();
 		$('#menu a').removeClass('selected');
 		elem.addClass('selected');
 		window.location.hash = elem.attr('href');
@@ -15,8 +15,8 @@ $(document).ready(function() {
 		$('#menu a[href=' + elem.attr('href') +']').click();
 	});
 
-	$('.content').hide();
-	$('.content:first').show();
+	$('.translation').hide();
+	$('.translation:first').show();
 	$('#menu a:first').addClass('selected');
 
 	var hash = window.location.hash;
